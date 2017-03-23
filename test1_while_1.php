d7551496907014cf263d967aeb33a9a8f72b1298
zn3sky <zn3sky@gmail.com>
2017-03-23 23:25:47 +0900

    create

---
+++ test1_while_1.php
@@ -0,0 +1,15 @@
+<?php
+echo "Current PHP version: " . phpversion() . "<br>";
+
+$start = microtime(true);
+
+$i = 0;
+while($i < 1000000) {
+    ++$i;
+}
+
+$end = microtime(true);
+
+$result = $end - $start;
+
+echo $result;
---
+++ test2_print.php
@@ -0,0 +1,17 @@
+<?php
+echo "Current PHP version: " . phpversion() . "<br>";
+
+$start = microtime(true);
+
+$i = 0;
+
+for ($i = 0; $i < 1000000; $i++) {
+	print a;
+}
+
+$end = microtime(true);
+
+$result = $end - $start;
+
+echo "<br>";
+echo $result;
