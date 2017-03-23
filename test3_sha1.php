b94785aef39b0d48d9efe4de116512a26d3ecb79
zn3sky <zn3sky@gmail.com>
2017-03-23 23:41:24 +0900

    create

---
+++ test3_sha1.php
@@ -0,0 +1,16 @@
+<?php
+echo "Current PHP version: " . phpversion() . "<br>";
+
+$start = microtime(true);
+
+$i = 0;
+
+for ($i = 0; $i < 1000000; $i++) {
+	$test = sha1("aaa");
+}
+
+$end = microtime(true);
+
+$result = $end - $start;
+
+echo $result;
